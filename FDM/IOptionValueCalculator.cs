ffe089507678a20761b3d5a63f2454c0a9f359c5
inawashiro <hiroina0926@icloud.com>
2019-08-14 07:22:12 +0900

    feat1: Solved error by introducing array feat2: Added a test class

---
+++ FDM/IOptionValueCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FDM
+{
+    public interface IOptionValueCalculator
+    {
+        double[,] CalculatePV(int spatialDivisionNum,
+                              int temporalDivisionNum,
+                              double boundaryOptionValue,
+                              double strike,
+                              double maturity,
+                              double domesticRate,
+                              double foreignRate,
+                              double volatility,
+                              bool isCall);
+    }
+}
