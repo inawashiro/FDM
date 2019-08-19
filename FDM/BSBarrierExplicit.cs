036b61f2b3d933f88be24e483cb6123244443fe2
inawashiro <hiroina0926@icloud.com>
2019-08-19 20:17:37 +0900

    feat1: Added BSBarrierExplicit.cs feat2: Added BSBarrierAnalytic.cs feat3: Added TestsBSBarrier.cs feat4: Renamed to TestsBSVanilla.cs

---
+++ FDM/BSBarrierExplicit.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FDM
+{
+    public class BSBarrierExplicit
+    {
+        private static void SetBoundaryCondition(int tNum, int xNum, double[,] pVArray, double boundaryPV, double strike, bool isCall)
+        {
+            for (int l = 0; l < tNum; l++)
+            {
+                int sign = isCall ? 1 : -1;
+
+                pVArray[l, 0] = 0;
+                pVArray[l, xNum - 1] = Math.Max(sign * (((double)xNum - 1) / (double)xNum * boundaryPV - strike), 0);
+            }
+        }
+        private static void SetInitialCondition(int xNum, double[,] pVArray, double boundaryPV, double strike, bool isCall)
+        {
+            for (int i = 1; i < xNum - 1; i++)
+            {
+                double initialPV = i * boundaryPV / xNum;
+
+                int sign = isCall ? 1 : -1;
+
+                pVArray[0, i] = Math.Max(sign * (initialPV - strike), 0);
+            }
+        }
+        public static double[,] CalculatePVArray(
+            int xNum,
+            int tNum,
+            double boundaryPV,
+            double strike,
+            double boundaryTime,
+            double domesticRate,
+            double foreignRate,
+            double volatility,
+            bool isCall)
+        {
+            double[,] pVArray = new double[tNum, xNum];
+
+            SetBoundaryCondition(tNum, xNum, pVArray, boundaryPV, strike, isCall);
+            SetInitialCondition(xNum, pVArray, boundaryPV, strike, isCall);
+
+            double dx = boundaryPV / xNum;
+            double dt = boundaryTime / tNum;
+
+            for (int l = 1; l < tNum; l++)
+            {
+                for (int i = 1; i < xNum - 1; i++)
+                {
+                    double initialPV = i * boundaryPV / xNum;
+
+                    double a1 = domesticRate * initialPV;
+                    double b11 = 0.5 * volatility * volatility * initialPV * initialPV;
+
+                    double a = b11 * dt / (dx * dx) - 0.5 * a1 * dt / dx;
+                    double b = 1 + domesticRate * dt - 2 * b11 * dt / (dx * dx);
+                    double c = b11 * dt / (dx * dx) + 0.5 * a1 * dt / dx;
+
+                    pVArray[l, i] =
+                        a * pVArray[l - 1, i - 1]
+                        + b * pVArray[l - 1, i]
+                        + c * pVArray[l - 1, i + 1];
+                }
+            }
+            return pVArray;
+        }
+    }
+}
---
+++ TestsBSVanillaExplicit/TestsBSBarrier.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Xunit;
+using FDM;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.Distributions;
+
+namespace FDM.Tests
+{
+    public class TestsBSBarrier
+    {
+        private static readonly int tNum = 10;
+        private static readonly int xNum = 10;
+        private static readonly double boundaryPV = 200;
+        private static readonly double strike = 100;
+        private static readonly double boundaryTime = 0.2;
+        private static readonly double domesticRate = 0.1;
+        private static readonly double foreignRate = 0.0;
+        private static readonly double volatility = 0.3;
+
+        [Fact]
+        public void BSBarrierTest()
+        {
+            double tol = 1e-3;
+            var isCall = true;
+
+            double squareAbsoluteErrorSum = 0;
+            double squareAnalyticSum = 0;
+            double relativeErrorTotal = 0;
+
+            for (int l = 1; l < tNum; l++)
+            {
+                double maturity = l * boundaryTime / tNum;
+
+                for (int i = 1; i < xNum - 1; i++)
+                {
+                    double initialPV = i * boundaryPV / xNum;
+
+                    var pVAnalytic =
+                        BSBarrierAnalytic.Make2DArray(
+                            xNum,
+                            tNum,
+                            boundaryPV,
+                            strike,
+                            boundaryTime,
+                            domesticRate,
+                            foreignRate,
+                            volatility,
+                            isCall);
+
+                    var pVFDM =
+                        BSBarrierExplicit.CalculatePVArray(
+                            xNum,
+                            tNum,
+                            boundaryPV,
+                            strike,
+                            boundaryTime,
+                            domesticRate,
+                            foreignRate,
+                            volatility,
+                            isCall);
+
+                    squareAbsoluteErrorSum += (pVAnalytic[l, i] - pVFDM[l, i]) * (pVAnalytic[l, i] - pVFDM[l, i]);
+                    squareAnalyticSum += pVAnalytic[l, i] * pVAnalytic[l, i];
+                }
+            }
+            relativeErrorTotal = squareAbsoluteErrorSum / squareAnalyticSum;
+            Assert.True(relativeErrorTotal < tol ? true : false);
+        }
+    }
+}
