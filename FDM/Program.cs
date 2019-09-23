64dcf0f9f51d7d5b9c3c90dfe9830ff5be7dfa9a
inawashiro <hiroina0926@icloud.com>
2019-09-23 23:17:43 +0900

    fix: Added a necessary file

--- FDM/Program.cs
+++ FDM/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            MakeConvergenceFileOneAsset.Complete();
+            //MakeConvergenceFileOneAsset.Complete();
             //MakeErrorDistributionFileOneAsset.Complete();
         }
     }
