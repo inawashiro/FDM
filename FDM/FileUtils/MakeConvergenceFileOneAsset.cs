9dce7dd82b167471079bb4630c176831f6824a46
inawashiro <hiroina0926@icloud.com>
2019-09-23 23:35:08 +0900

    fix: Added necessary files

---
+++ FDM/FileUtils/MakeConvergenceFileOneAsset.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+using System.IO;
+
+namespace FDM
+{
+    public static class MakeConvergenceFileOneAsset
+    {
+        public static void Complete()
+        {
+            var file =
+                new StreamWriter(
+                    @"/Users/hiromichi/Desktop/official/job/advanced/data/Vanilla/Convergence.csv",
+                    false,
+                    Encoding.UTF8);
+            MergeIndexAndValue(file, Types.OptionType.Vanilla);
+
+            file =
+                new StreamWriter(
+                    @"/Users/hiromichi/Desktop/official/job/advanced/data/Barrier/Convergence.csv",
+                    false,
+                    Encoding.UTF8);
+            MergeIndexAndValue(file, Types.OptionType.Barrier);
+        }
+
+        private static void MergeIndexAndValue(
+            StreamWriter file,
+            Types.OptionType optionType)
+        {
+            WriteIndex(file);
+            WriteError(file, optionType, Types.MethodType.Explicit);
+            WriteError(file, optionType, Types.MethodType.Implicit);
+            WriteError(file, optionType, Types.MethodType.CrankNicolson);
+            file.Close();
+        }
+
+        private static void WriteIndex(StreamWriter file)
+        {
+            var xNumArray = ParametersFactory.MakeXNumArray();
+            int xNumNum = xNumArray.GetLength(0);
+
+            file.Write(",");
+            for (int j = 0; j < xNumNum; j++)
+            {
+                file.Write(xNumArray[j] + ",");
+            }
+            file.WriteLine();
+        }
+
+        private static void WriteError(StreamWriter file, Types.OptionType optionType, Types.MethodType methodType)
+        {
+            var parameters = ParametersFactory.ForVerification(optionType);
+            var xNumArray = ParametersFactory.MakeXNumArray();
+            int xNumNum = xNumArray.GetLength(0);
+
+            file.Write(",");
+            for (int j = 0; j < xNumNum; j++)
+            {
+                var makePVArray = new MakePVArrayOneAsset();
+                var analyticArray =
+                    makePVArray.Analytic(parameters[j], optionType);
+                var fDMArray =
+                    makePVArray.FDM(parameters[j], optionType, methodType);
+                double error = CalculateErrorOneAsset.MaxAbsolute(fDMArray, analyticArray);
+
+                file.Write(error + ",");
+            }
+            file.WriteLine();
+        }
+    }
+}
--- FDM/Program.cs
+++ FDM/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            //MakeConvergenceFileOneAsset.Complete();
-            //MakeErrorDistributionFileOneAsset.Complete();
+            MakeConvergenceFileOneAsset.Complete();
+            MakeErrorDistributionFileOneAsset.Complete();
         }
     }
 }
